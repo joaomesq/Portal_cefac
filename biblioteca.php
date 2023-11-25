645c86e41b52c0bd4b4c1f1c51e24ab74cc45d52
joaomesquita1 <joaob.mesquita1@gmail.com>
2023-11-25 09:55:20 -0200

    Create function upload_apresentacao

--- biblioteca.php
+++ biblioteca.php
@@ -1,8 +1,49 @@
 <?php
 //conexao
 require_once './php_action/conect.php';
- 
 
+//UPLOAD APRESENTACAO
+function ulpoad_apresentacao(){
+    global $conect;
+
+    $disciplina = clear($_POST['disciplina']);
+
+    if($_FILES['arquivo_apresentacao']['size'] == 0)
+        die("Selecione o arquivo");
+
+    if(empty($disciplina))
+        die("Precisa preenche o campo disciplina");
+
+
+    if(isset($_FILES['arquivo_apresentacao'])):
+        $apresentacao = $_FILES['arquivo_apresentacao'];
+        $pasta = 'apresentacao/';
+
+        if($apresentacao['error'])
+            die("Fallha ao carregar arquivo");
+
+        $nome_apresentacao = $apresentacao['name'];
+        $novo_nome_livro = uniqid();
+        $extensao = strtolower(pathinfo($nome_apresentacao, PATHINFO_EXTENSION));
+        $caminho = $pasta.$nome_apresentacao.".".$extensao;
+
+        if($extensao != 'pptx' AND $extensao != 'ppt' AND $extensao != 'pps' AND $extensao != 'potx' AND $extensao != 'ppsx')
+            die("Tipo de arquivo não aceito");
+
+        $sucesso = move_uploaded_file($apresentacao['tmp_name'], $caminho);
+
+        if($sucesso):
+            $sql = "INSERT INTO apresentacao (titulo_apresentacao, disciplina_apresentacao, data_upload_apresentacao, caminho_apresentacao) VALUES ('$nome_apresentacao', '$disciplina', NOW(), '$caminho')";
+            $consulta = mysqli_query($conect, $sql);
+            echo "Livro carregado com sucesso para acessar, clique aqui: <a href='$caminho' target='_blank'>Clique Aqui</a>";
+        else:
+            echo "Fallha ao enviar arquivo";
+        endif;
+    endif;
+} 
+
+
+//UPLOAD LIVROS
 function ulpoad_livros(){
     global $conect;
 
@@ -48,6 +89,11 @@ function ulpoad_livros(){
  if (isset($_POST['carregar_livro'])) {
     ulpoad_livros();
  }
+
+ //...Apresentação
+ if(isset($_POST['carregar_apresentacao'])):
+    ulpoad_apresentacao();
+endif;
 ?>
 
 <!DOCTYPE html>
@@ -101,7 +147,7 @@ function ulpoad_livros(){
                 </table>
 
                 <button name="adicionar_apresentacao">Adicionar</button>
-                <form action="#" method="#" name="apresentacao" enctype="multipart/form-data">
+                <form action="" method="POST" name="apresentacao" enctype="multipart/form-data">
                     <fieldset>
                         <legend>Adicionar Apresentação</legend>
 
